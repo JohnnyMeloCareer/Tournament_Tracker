518b56382b60d8d9049252c90681bd698296e678
Johnny Melo <Johnnymelowork@gmail.com>
2019-01-25 12:34:04 -0500

    TournamentCards index.js export

--- client/tournament-tracker/src/App.js
+++ client/tournament-tracker/src/App.js
@@ -5,12 +5,14 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/Navbar';
+import TournamentCards from './components/TournamentCards';
 
 
 const App = () => (
 
   <Router>
     <Navbar />
+    <TournamentCards /> 
     </Router >
   )
 
