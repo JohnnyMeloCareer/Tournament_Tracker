42b6ace5c301b9a4a6cd9564812026bf17f157a3
Johnny Melo <Johnnymelowork@gmail.com>
2019-01-25 23:24:53 -0500

    Tournament Card

---
+++ client/src/components/TournamentCards/TournamentCards.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Card, CardImg, CardText, CardBody,
+  CardTitle, CardSubtitle, Button } from 'reactstrap';
+  import './TournamentCards.css';
+
+const TournamentCards = () => {
+  return (
+    <div>
+      <Card>
+        <CardImg top width="100%" src="https://media.discordapp.net/attachments/275410596164403202/538571479130963979/stone_demo.PNG" alt="Card image cap" />
+        <CardBody>
+          <CardTitle>Card title</CardTitle>
+          <CardSubtitle>Card subtitle</CardSubtitle>
+          <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+          <Button>Join</Button>
+        </CardBody>
+      </Card>
+    </div>
+  );
+};
+
+export default TournamentCards;
